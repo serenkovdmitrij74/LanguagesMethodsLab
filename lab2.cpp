54e3b74db4fb4b52522d747be2c845ddd4377fd9
Dmitrij74 <111426478+serenkovdmitrij74@users.noreply.github.com>
2025-10-20 11:35:57 +0300

    Update lab2.cpp

--- lab2.cpp
+++ lab2.cpp
@@ -21,20 +21,22 @@ vector<int> create_vector() {
 }
 
 int interpolation_search(vector<int> vec) {
-    int s, a=0, b=vec.size()-1, m=0;
+    int s, a = 0, b = vec.size() - 1, m = 0;
     printf("Enter the number to search: ");
     cin >> s;
 
-    while (a <= b && s >= vec[a] && s <= vec[b]) {
+    while (s >= vec[a] && s <= vec[b]) {
         if (vec[m] == s)
             return m;
+        if (vec[a] == vec[b])
+            break;
 
         m = (s - vec[a]) * (b - a) / (vec[b] - vec[a]) + a;
-        
+
         if (vec[m] < s)
-            a = m+1;
-        else if (vec[m]>s)
-            b = (m+1);
+            a = m + 1;
+        else if (vec[m] > s)
+            b = m - 1;
     }
 
     printf("Element %d is not in this array\n", s);
